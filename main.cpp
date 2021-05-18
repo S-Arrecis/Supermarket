b541102884baefef1219025b4cd2b2986b6442d3
Arrecis <arsteven9@gmail.com>
2021-05-18 17:47:06 -0600

    Agregando metodo principal

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//#include "ConexionBD.h"
+#include "Persona.h"
+#include "Empleado_Cliente.h"
+#include "Marca.h"
+#include "Producto.h"
+#include "Proveedor.h"
+#include "Puesto.h"
+
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
